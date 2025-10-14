b902d0ddb62f468e8e9faa62bb41a0fa26289542
Rob Anderson <robandpdx@github.com>
2025-10-14 17:32:29 -0400

    increase function timeout to 10 seconds

--- amplify/functions/start-migration/resource.ts
+++ amplify/functions/start-migration/resource.ts
@@ -3,6 +3,7 @@ import { defineFunction } from '@aws-amplify/backend';
 export const startMigration = defineFunction({
   name: 'start-migration',
   entry: './handler.ts',
+  timeoutSeconds: 10,
   environment: {
     TARGET_ORGANIZATION: process.env.TARGET_ORGANIZATION || '',
     SOURCE_ADMIN_TOKEN: process.env.SOURCE_ADMIN_TOKEN || '',
